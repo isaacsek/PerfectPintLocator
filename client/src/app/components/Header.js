231f9ec72f109880e44984157a8c23d052a42203
isek27 <isaacsek@uw.edu>
2018-04-01 01:14:55 -0700

    remove stripe

--- client/src/app/components/Header.js
+++ client/src/app/components/Header.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import Payments from "./Payments";
 
 class Header extends Component {
 
@@ -13,8 +12,6 @@ class Header extends Component {
                 return <li><a href="/auth/google">Login With Google</a></li>;
             default:
             return [
-                <li key="1"><Payments /></li>,
-                <li key="2" style={{margin: '0 10px'}}>Credits: {this.props.auth.credits}</li>,
                 <li key="3"><a href="/api/logout">Logout</a></li>
             ];
         }
